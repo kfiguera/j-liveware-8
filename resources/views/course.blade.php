cbf9b34ed9c5e14bfb0b9dbb1d67083f4f28665f
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-08 10:41:06 -0400

    Estructura inicial pagina del curso individual

---
+++ resources/views/course.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase ">Detalle del Curso</h1>
+    <h2 class="text-xl text-gray-600 ">Fórmate online como profesional en tecnología</h2>
+</div>
+    <livewire:course-list>
+@endsection

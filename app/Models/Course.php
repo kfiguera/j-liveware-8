8c601a53e360a5521d26613bce3ebec578390489
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-08 12:54:56 -0400

    Cursos Similares

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -22,4 +22,13 @@ class Course extends Model
     {
         return substr($this->description,0,140);
     }
+    public function similar()
+    {
+        $courses = $this->where('category_id',$this->category_id)
+            ->where('id','<>',$this->id)
+            ->with('user')
+            ->take(2)
+            ->get();
+        return $courses;
+    }
 }
--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="grid grid-cols-3 gap-4">
-        <div class="p-8 bg-gray-200 col-span-1">
+        <div class="p-8 bg-gray-200 col-span-1 rounded-xl">
             <ul class="flex flex-col">
                 <li class="font-medium text-sm text-gray-400 uppercase mb-4"> Contenido</li>
                 @foreach($course->posts as $post)
@@ -15,22 +15,43 @@
                 @endforeach
             </ul>
         </div>
-        <div class="text-gray-700 col-span-2">
-            <img src="{{ $course->image }}">
-            <h2 class="text-2xl">{{ $course->name }}</h2>
-            <p>{{ $course->description }}</p>
-            <div class="flex mt-3 items-center">
-                <img src="{{ $course->user->avatar }}" class="w-10 h-10 rounded-full mr-2">
-                <div>
+        <div class="text-gray-700 col-span-2 rounded-xl">
+            <img src="{{ $course->image }}" class="rounded-xl">
+            <div class="flex my-2 items-center">
+                <h2 class="text-2xl my-3">{{ $course->name }}</h2>
+                <img src="{{ $course->user->avatar }}" class="w-10 h-10 rounded-full ml-auto">
+                <div class="ml-2">
                     <p class="text-gray-500 text-sm">{{ $course->user->name }}</p>
                     <p class="text-gray-300 text-xs">{{ $course->created_at->diffForHumans() }}</p>
                 </div>
+
             </div>
+            <p>{{ $course->description }}</p>
+
+
+            <h2 class="text-2xl mt-2">Cursos Similares</h2>
+            <div class="grid grid-cols-2 gap-4">
+                @foreach($course->similar() as $course)
+                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+                        <a href="{{ route('course',$course->slug) }}">
+                            <img src="{{ $course->image }}" class="rounded-md mb-2 ">
+                            <h2 class="text-lg font-bold truncate uppercase">{{ $course->name }}</h2>
+                            <p>{{ $course->get_excerpt }}</p>
+                            <div class="flex justify-center items-center mt-4">
+                                <img src="{{ $course->user->avatar }}" class="rounded-full h-16 w-16">
+                                <p class="ml-4 text-gray-500 uppercase">{{ $course->user->name }}</p>
+                            </div>
+
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
         </div>
     </div>
     <div class="text-center mt-4">
 
-        <h1 class="text-3xl text-gray-700 mb-2 uppercase ">Detalle del Curso</h1>
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase ">Ultimos Cursos</h1>
         <h2 class="text-xl text-gray-600 ">Fórmate online como profesional en tecnología</h2>
     </div>
     <livewire:course-list>

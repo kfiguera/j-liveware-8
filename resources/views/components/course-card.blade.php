5fee2286d60dd6f21eb4dc03adaab94217bf0a18
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-08 13:10:51 -0400

    Implementación del componente course-card para simplificación del código

---
+++ resources/views/components/course-card.blade.php
@@ -0,0 +1,12 @@
+<div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+    <a href="{{ route('course',$course->slug) }}">
+        <img src="{{ $course->image }}" class="rounded-md mb-2 ">
+        <h2 class="text-lg font-bold truncate uppercase">{{ $course->name }}</h2>
+        <p>{{ $course->get_excerpt }}</p>
+        <div class="flex justify-center items-center mt-4">
+            <img src="{{ $course->user->avatar }}" class="rounded-full h-16 w-16">
+            <p class="ml-4 text-gray-500 uppercase">{{ $course->user->name }}</p>
+        </div>
+
+    </a>
+</div>
--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -32,18 +32,7 @@
             <h2 class="text-2xl mt-2">Cursos Similares</h2>
             <div class="grid grid-cols-2 gap-4">
                 @foreach($course->similar() as $course)
-                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
-                        <a href="{{ route('course',$course->slug) }}">
-                            <img src="{{ $course->image }}" class="rounded-md mb-2 ">
-                            <h2 class="text-lg font-bold truncate uppercase">{{ $course->name }}</h2>
-                            <p>{{ $course->get_excerpt }}</p>
-                            <div class="flex justify-center items-center mt-4">
-                                <img src="{{ $course->user->avatar }}" class="rounded-full h-16 w-16">
-                                <p class="ml-4 text-gray-500 uppercase">{{ $course->user->name }}</p>
-                            </div>
-
-                        </a>
-                    </div>
+                    <x-course-card :course="$course" />
                 @endforeach
             </div>
 

411cbffdae027df5da182b2ea44bdc8b70227f7e
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-07 22:34:34 -0400

    Index y layout: complementar estructura

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -11,11 +11,29 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    <main class="py-10">
-        <div class="container mx-auto px-4">
-            @yield('content')
-        </div>
-    </main>
+<header class="shadow-lg">
+    <div class="bg-white py-1">
+        <nav class="py-2">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('images/logo.png') }}" class="h-10 mx-auto">
+            </a>
+        </nav>
+    </div>
+
+</header>
+<main class="py-10">
+    <div class="container mx-auto px-4">
+        @yield('content')
+    </div>
+</main>
+<footer class="py-4 text-center">
+    @auth
+        <a href="{{ url('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+    @else
+        <a href="{{ url('login') }}" class="text-sm text-gray-700 underline mr-3">Login</a>
+        <a href="{{ url('register') }}" class="text-sm text-gray-700 underline">Register</a>
+    @endif
+</footer>
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
